26981766c944595ffc205bac3c0c19fad73a883c
Amine Amari <amari.mohamed.amine90@gmail.com>
2022-12-14 22:27:43 +0100

    implemented pagination

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -4,12 +4,16 @@ import Character from './Character';
 
 export default function Characters() {
     
-    const fetchCharacters = async () => {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
+    const [page, setPage] = useState(42);
+
+    const fetchCharacters = async ({queryKey}) => {
+        const response = await fetch(
+            `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+            );
         return response.json();
     }; 
 
-    const {data, status} =  useQuery("characters", fetchCharacters);
+    const {data, status} =  useQuery(["characters", page], fetchCharacters);
 
     if (status === "loading" ) {
         return <div>Loading..</div>
@@ -23,6 +27,18 @@ export default function Characters() {
             {data.results.map((character) => (
                 <Character character ={character}/>
             ))}
+            <div>
+                <button 
+                    disabled={page === 1} 
+                    onClick={() => setPage((old) => (old-1))}
+                    >
+                        Previous</button>
+                <button
+                    disabled={!data.info.next} 
+                    onClick={() => setPage((old) => (old+1))}
+                    >
+                        Next</button>
+            </div>
         </div>
     )
 }
